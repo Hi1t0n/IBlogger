4c46267f1ced6a6d7c18056c67d80a8af9cdbd0b
HILTON <artyomhilton@yahoo.com>
2025-03-21 00:31:25 +0300

    Add comment

--- Backend/PostService/PostService.Host/Extensions/WebApplicationExtensions.cs
+++ Backend/PostService/PostService.Host/Extensions/WebApplicationExtensions.cs
@@ -3,8 +3,15 @@ using PostService.Infrastructure.Context;
 
 namespace PostService.Host.Extensions;
 
+/// <summary>
+/// Методы расширения для класса <see cref="WebApplication"/>.
+/// </summary>
 public static class WebApplicationExtensions
 {
+    /// <summary>
+    /// Применение миграции к бд.
+    /// </summary>
+    /// <param name="webApplication"><see cref="WebApplication"/>.</param>
     public static async void ApplyMigrations(this WebApplication webApplication)
     {
         using var scope = webApplication.Services.CreateScope();
